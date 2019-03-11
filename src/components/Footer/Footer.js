60a6d1d4421a5a616bb7c252c9bbcc367cf0a6d3
Nastaran <nastaran.eskandari@gmail.com>
2019-03-11 13:08:52 -0400

    update

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Footer.css';
 // import ScrollAnimation from 'react-animate-on-scroll';
 import ScrollableAnchor from 'react-scrollable-anchor';
+import Mailto from 'react-protected-mailto'
 
 export class Footer extends React.Component{
     render(){
@@ -11,7 +12,7 @@ export class Footer extends React.Component{
                     <div className="footer">
                         <nav className="footer_navbar">
                             <div className="footer_navbar--info">
-                                <a href="mailto :nastaran.eskandari@gmail.com" ><p>nastaran.eskandari@gmail.com</p></a>
+                             <p>Email: <Mailto email='nastaran.eskandari@gmail.com'/></p>
                             </div>
                             <div className="footer_navbar--social">
                                 <a href="https://www.linkedin.com/in/nastaran-eskandari/"  rel="noopener noreferrer" target="_blank"><i className="fab fa-linkedin fa-2x"></i></a>
